63f1688de56fadd9a456b70d1c962a7cdafb7dd0
bekbol <zhalilovbekbol@gmail.com>
2022-10-07 16:49:28 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+class Main<a, b> {
+    public static void main(String[] args) {
+
+
+
+        String[]names= {"Masha","Natasha","Sveta"};
+         for( String a:names){
+             System.out.println("men seny jakshy korom"+a);
+
+   /**     san(15);
+    }
+    static boolean san(int a){
+        return (true (a,));*/
+
+        }
+
+/**System.out.println(oddOrEven(11));
+
+}
+    static boolean oddOrEven(int a){
+        return (a % 2 == 0);*/
+
+    }
+    }
